05d547daa76d218d4030e8782f62cc2d7c394ebd
Diep Nguyen <diepniit.nguyenhong@gmail.com>
2020-06-18 16:29:13 +0700

    Create new repo for Nop-Commerce project

---
+++ actions/commons/GlobalConstants.java
@@ -0,0 +1,12 @@
+package commons;
+
+public class GlobalConstants {
+
+	public static String PROJECT_FOLDER = System.getProperty("user.dir");
+	public static String NOPCOMMERCE_URL = "https://demo.nopcommerce.com/";
+	public static String USERNAME = "automationeditor";
+	public static String PASSWORD = "automationfc";
+	public static long LONG_TIMEOUT = 30;
+	public static long SHORT_TIMEOUT = 5;
+	
+}
